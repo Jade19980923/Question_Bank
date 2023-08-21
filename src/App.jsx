58e5645b435385cfba4cd3b6b6963dc96cb9d7b7
Edward <lisong95119@163.com>
2023-08-22 10:55:08 +1200

    22/Aug

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import Header from './components/Header'
 import AuthRouter from './components/AuthRouter'
+import NavigationBar from './components/NavigationBar'
 import BottomBar from './components/BottomBar'
-import { useRoutes, Navigate, BrowserRouter as Router } from 'react-router-dom'
+import { useRoutes, Navigate, BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
+import Background from './components/Background'
+
+
+
 
 function App() {
   return (
     <Router>
       <div className="app-box">
         <Header></Header>
+
+        <NavigationBar></NavigationBar>
         <AuthRouter></AuthRouter>
+        <Background></Background>
         <BottomBar></BottomBar>
+        
       </div>
+      
     </Router>
 
   );
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,10 @@
+import { Link } from 'react-router-dom';
 import './index.css';
 
 function Login() {
+    const onCreateNewAccount = () => {
+
+    }
     return (
         <div className="login-box">
             <div className='login-content'>
@@ -13,15 +17,19 @@ function Login() {
                     <div className='login-content-left-option'>Password</div>
                     <input type="password"></input>
                     <div className='login-content-left-forget'>Forgot password?</div>
-                    <div className='login-content-left-login'>Login</div>
+                    <link to="/Home">
+                        Login
+                    </link>
                 </div>
                 <div className='divider'></div>
                 <div className='login-content-right'>
                     <div className='login-content-right-part'>New to Question Bank</div>
                     <div className='login-content-right-content'>Only create a new account if you are new to Question Bank. During registration, we will ask you minimal personal information sowe can confirm you as a unique user.</div>
-                    <div className='login-content-create'>Create new account</div>
+                    <Link to="/signUp">
+                        <div className='login-content-create' onClick={onCreateNewAccount}>Create new account</div>
+                    </Link>
                     <div className='login-content-right-part'>Need help?</div>
-                    <div className='login-content-right-content'>If you have trouble logging in, or if you need to update your personal information or school/organization affiliation, please contact us</div>
+                    <div className='login-content-right-content'>If you have trouble logging in, or if you need to update your personal information or school/organization affiliation, please contact us.</div>
                 </div>
             </div>
         </div>
